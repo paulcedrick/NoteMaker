61e3d4cc3596d23240e13c218cbb31bbb77906d6
Paul Cedrick Artigo <paulcedrick.artigo@gmail.com>
2017-01-04 22:58:13 +0800

    created a unit test for getNoteById and update function

--- test/lib/notes.spec.js
+++ test/lib/notes.spec.js
@@ -36,27 +36,44 @@ test('NotesLib.create', t => {
   });
 });
 
-test('NotesLib.getNoteById should return a promise', t => {
+test('NotesLib.getNoteById', t => {
   let params = {
     id: 1
   };
+  let obj = NotesLib.getNoteById(params.id);
+  t.ok(obj && typeof obj.then == 'function', 'should return a promise');
 
-  NotesLib.getNoteById(1)
-  .then(note => {
-    t.ok(params.id == note.id);
-    t.end();  
+  params.id = null;
+  NotesLib.getNoteById(params.id)
+  .catch(e => {
+    t.equal(e.errorNo, 400, 'should validate the parameter');
+    t.end();
   });
   
 });
 
-// test('NotesLib.update should return a promise', t => {
-//   let params = {
-//     id: 1,
-//     title: 'alsdkajsdkl',
-//     body: 'Lorem Ipsum Ipsum' 
-//   };
-//   let obj = NotesLib.update(params);
+test('NotesLib.update', t => {
+  let params = {
+    id: 1,
+    title: 'alsdkajsdkl',
+    body: 'Lorem Ipsum Ipsum' 
+  };
+  let obj = {};
+  let fakenote = {};
+  fakenote.save = sinon.spy();
 
-//   t.ok(obj && obj.then == 'function');
-//   t.end();
-// });
+  NotesLib.update(fakenote, params);
+  t.ok(fakenote.save.calledOnce, 'should call the save function of note object');
+
+
+  NotesLib.getNoteById(params.id)
+  .then(note => {
+    obj = NotesLib.update(note, params);
+    return obj;
+  })
+  .then((note) => {
+    t.ok(obj && typeof obj.then == 'function', 'should return a promise');
+    t.ok(note.get('id') === params.id, 'should return the edited item');
+    t.end();
+  });
+});
